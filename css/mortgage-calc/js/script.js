3613edca4aaa129ce60396dbbb21a18304966d22
Ben Lang <lang.b1@stu.janesville.k12.wi.us>
2022-12-02 15:53:36 -0600

    update

--- css/mortgage-calc/js/script.js
+++ css/mortgage-calc/js/script.js
@@ -1,1 +1,19 @@
+function calc() {
+var P = document.getElementById("P").value;
+var r = document.getElementById("r").value;
+var t = document.getElementById("t").value;
 
+
+
+
+<button onclick="
+    var P = document.getElementById('P').value;
+    var n = 12;
+    var r = document.getElementById('r').value;
+    var t = document.getElementById('t').value;
+    var m = document.getElementById('m').value;
+    var B = P * (1 +  r / n) ** (n * t) - m * [(1 + r / n) ** n * t - 1] / (n / 12);
+    B = B.toFixed(2);
+    alert ('Mortgage Balance: ' + B);
+">Calculate</button>
+}
